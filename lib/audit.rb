ff73e624951fb30f1aea38d2f95a68456c8f0e6a
jrambold <jrambold@gmail.com>
2018-02-27 12:00:45 -0700

    employee and project invalid added

--- lib/audit.rb
+++ lib/audit.rb
@@ -8,4 +8,43 @@ class Audit
   def load_company(company)
     @company = company
   end
+
+  def were_invalid_days_worked
+    invalid_employees = invalid_employee_ids
+    invalid_projects = invalid_project_ids
+    build_report(invalid_employees,invalid_projects)
+  end
+
+  def build_report(employees, projects)
+    report = "Invalid Days Worked:\n"
+    employees.each do |timesheet|
+      project = @company.find_project_by_id(timesheet.project_id).name
+      report += "#{timesheet.employee_id} ID worked on #{project} "
+      report += "on #{timesheet.date}, Employee ID is invalid"
+    end
+    projects.each do |timesheet|
+      name = @company.find_employee_by_id(timesheet.employee_id).name
+      project = @company.find_project_by_id(timesheet.project_id).name
+      report += "#{name} worked on #{project} on #{timesheet.date}, "
+      report += "Project ID is invalid"
+    end
+  end
+
+  def invalid_employee_ids
+    invalid_employees = []
+    @company.timesheets.each do |timesheet|
+      unless @company.employees.include?(timesheet.employee_id)
+        invalid_employees << timesheet
+      end
+    end
+  end
+
+  def invalid_project_ids
+    invalid_projects = []
+    @company.timesheets.each do |timesheet|
+      unless @company.employees.include?(timesheet.project_id)
+        invalid_projects << timesheet
+      end
+    end
+  end
 end
--- test/audit_test.rb
+++ test/audit_test.rb
@@ -1,6 +1,7 @@
 require './test/test_helper'
 require './lib/company'
 require './lib/audit'
+require './lib/timesheet'
 
 class AuditTest < Minitest::Test
   def setup
@@ -19,5 +20,27 @@ class AuditTest < Minitest::Test
 
   def test_load_company
     assert_equal @company, @audit.company
+  end
+
+  def test_invalid_days_worked
+    skip
+    assert_equal "Invalid Days Worked:\nNone", @audit.were_invalid_days_worked
+  end
+
+  def test_build_report
+    employee_id = '1'
+    project_id = '1'
+    date = '2015-01-01'
+    minutes = '120'
+    timesheet0 = Timesheet.new(employee_id, project_id, date, minutes)
+
+    employee_id = '2'
+    project_id = '2'
+    date = '2015-01-01'
+    minutes = '120'
+    timesheet1 = Timesheet.new(employee_id, project_id, date, minutes)
+
+    expected = " "
+    assert_equal expected, @audit.build_report([timesheet0],[timesheet1])
   end
 end
